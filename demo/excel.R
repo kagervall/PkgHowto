0840973044391258578335d402115b732ffe5131
Anders Kagervall <anders.kagervall@gmail.com>
2025-02-27 16:00:01 +0100

    Fix typo

--- demo/excel.R
+++ demo/excel.R
@@ -5,6 +5,6 @@ library(dplyr)
 ## Read many excel files and combine them into a single data frame
 fnames <- c("file1.xlsx", "file2.xlsx", "file3.xlsx")
 result_list <- lapply(fnames, read_xlsx)
-result_df <-  bind_rows(res)
+result_df <-  bind_rows(result_list)
 
 
